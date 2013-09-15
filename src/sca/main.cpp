3582c7fef2b2e2786983e065fff59b4c26ad69a6
Kirill Krinkin <kirill.krinkin@gmail.com>
2013-09-16 01:59:43 +0400

    #4355 Created project sources

---
+++ src/sca/main.cpp
@@ -0,0 +1,15 @@
+// Comments go here TBD
+
+
+#include <QApplication>
+#include "mainwindow.h"
+
+int main(int c, char **v)
+{
+    QApplication app(c,v);
+
+    MainWindow mw;
+    mw.show();
+
+    return app.exec();
+}
