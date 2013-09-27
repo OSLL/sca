b9eed9d467921f45fd0eb4bcc18a23e8cf31637b
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2013-09-27 17:53:04 +0400

    #4366: classes can be generated without namespace

--- templates/derive.sh
+++ templates/derive.sh
@@ -1,8 +1,29 @@
 #!/bin/sh
 # $Id$
 
-if [ -z "$1" ]; then echo 'Namespace:'; read PACKAGE ; else PACKAGE=$1 ; fi
-if [ -z "$2" ]; then echo 'Class name:'; read NM ; else NM=$2 ; fi
+
+if [ $# = "2" ]
+then
+	PACKAGE=$1;
+	NM=$2;
+	H_FILE="template.h.t";
+	CPP_FILE="template.cpp.t";
+else 
+	if [ $# = "1" ]
+	then
+		NM=$1;
+		H_FILE="template_without_namespace.h.t";
+		CPP_FILE="template_without_namespace.cpp.t";
+	else 
+		echo "Usage: ./derive.sh namspace className"
+		echo "or"
+		echo "./derive.sh className"
+		exit 0
+	fi
+fi
+
+#if [ -z "$1" ]; then echo 'Namespace:'; read PACKAGE ; else PACKAGE=$1 ; fi
+#if [ -z "$2" ]; then echo 'Class name:'; read NM ; else NM=$2 ; fi
 
 UUID=`uuidgen | tr 'abcdef-' 'ABCDEF_'`
 AUTHOR=`git config --get user.name`
@@ -15,17 +36,15 @@ NAME=$NM
 NAMEH=$NM.h
 NAMECPP=$NM.cpp
 
-PROJECT=
-PROJECT_NAMESPACE=
 
 A="cat <<EOF
-`cat template.h.t`
+`cat $H_FILE`
 EOF"
 
 eval "$A" >$NAMEH
 
 A="cat <<EOF
-`cat template.cpp.t`
+`cat $CPP_FILE`
 EOF"
 
 eval "$A" >$NAMECPP
